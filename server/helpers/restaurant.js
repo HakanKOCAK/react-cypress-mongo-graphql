1003f20e5edb0a91de6ed5c6e143d7d186741280
unknown <hknkocak97@icloud.com>
2022-01-18 06:44:21 +0300

    Do not create restaurants again if created before

--- server/helpers/restaurant.js
+++ server/helpers/restaurant.js
@@ -6,11 +6,8 @@ import { createMenu } from "./menu.js";
 const require = createRequire(import.meta.url);
 const restaurants = require("../restaurants.json");
 
-export const createDefaultRestaurants = async () => {
+const createRestaurants = async () => {
   try {
-    await Restaurant.deleteMany({});
-    await Menu.deleteMany({});
-
     //Update created at entry of each restaurant -> will be used for sorting.
     const copyRestaurants = _.cloneDeep(restaurants).map((r, index) => {
       return {
@@ -21,7 +18,14 @@ export const createDefaultRestaurants = async () => {
 
     const rests = await Restaurant.create(copyRestaurants);
     console.log("Default Restaurants created");
+    return rests;
+  } catch (error) {
+    throw error;
+  }
+};
 
+const createMenus = async ({ rests = [] }) => {
+  try {
     const menus = [];
     rests.forEach((rest) => {
       const menu = createMenu({ cuisine: rest.cuisine });
@@ -30,7 +34,20 @@ export const createDefaultRestaurants = async () => {
 
     await Menu.create(menus);
     console.log("Restaurants' menu's created");
-    return rests;
+  } catch (error) {
+    throw error;
+  }
+}
+export const createDefaultRestaurants = async () => {
+  try {
+    const dbRestaurants = await Restaurant.find({});
+    const dbMenus = await Menu.find({});
+    if (dbRestaurants.length === 0) {
+      const rests = await createRestaurants();
+      await createMenus({ rests });
+      return rests;
+    }
+    return dbRestaurants;
   } catch (error) {
     throw error;
   }
@@ -40,6 +57,21 @@ export const getDeliveryDetailsOfDistrict = ({ restaurant = {}, district = '' })
   return restaurant.deliveryDetails[district] || {};
 };
 
+export const getRestaurantById = async ({ id = '', district = '' }) => {
+  try {
+    const restaurant = await Restaurant.findById(id);
+    if (restaurant) {
+      return {
+        ...restaurant._doc,
+        id: restaurant.id,
+        deliveryDetails: getDeliveryDetailsOfDistrict.bind(this, { restaurant, district })
+      }
+    }
+    return null;
+  } catch (error) {
+    throw error;
+  }
+}
 
 export const getRestaurants = async ({ city = '', county = '', district = '' }) => {
   try {
